7b9f6f9950321ea8699e4c8b8625e9332c14ab71
Len Smith <len@barrison.com>
2016-09-25 16:30:37 -0400

    style changes. add fascist key

--- data.js
+++ data.js
@@ -1,16 +1,38 @@
 var data = {
   prefixes: {
-    crazy: "Donald Trump believes"
+    crazy: "Donald Trump believes",
+    fascist: "Donald Trump is a fascist"
   },
-  crazy: [
+  fascist: [
     {
-      title: "Global Warming is \"bullshit\" and temperatures are at record lows",
+      title: "openly calls for the U.S. to commit war crimes, targeting innocent women and children for murder",
       urls: [
-        "https://twitter.com/realDonaldTrump/status/418542137899491328?ref_src=twsrc%5Etfw"
+        "http://thinkprogress.org/politics/2015/12/15/3732671/trump-isis-kill-family-members/",
+        "http://www.independent.co.uk/news/world/americas/donald-trump-reiterates-desire-to-murder-terrorists-families-a6912496.html",
+        "http://www.cnn.com/2015/12/02/politics/donald-trump-terrorists-families/",
+        "http://www.washingtontimes.com/news/2016/mar/3/donald-trump-says-hed-force-us-military-commit-war/"
       ]
     },
     {
-      title: "Global Warming is a hoax by The Chinese",
+      title: "after the military balked, he promised to make them commit war crimes",
+      urls: [
+        "http://www.washingtontimes.com/news/2016/mar/3/donald-trump-says-hed-force-us-military-commit-war/",
+        "http://www.politifact.com/truth-o-meter/statements/2015/dec/17/rand-paul/rand-pauls-right-geneva-conventions-bar-donald-tru/",
+        "http://theweek.com/speedreads/610361/donald-trump-doubles-down-torture-war-crimes",
+        "http://www.telesurtv.net/english/opinion/The-US-Military-Will-Commit-Donald-Trumps-War-Crimes-20160307-0011.html",
+        "http://reason.com/blog/2016/03/03/donald-trump-says-military-wont-refuse-h",
+        "http://www.rawstory.com/2016/03/a-retired-major-general-sets-trump-straight-after-he-advocates-war-crimes/",
+        "http://www.democracynow.org/2016/3/1/donald_trump_wants_to_commit_war"
+      ]
+    },
+    {
+      title: "on torture he thinks \"Even if it doesn't work they probably deserved it anyway\"",
+      urls: ["http://www.cnn.com/2016/03/11/opinions/trump-wrong-on-american-torture/", "http://www.cnsnews.com/news/article/patrick-goodenough/trump-waterboarding-it-works-if-it-doesnt-work-they-deserve-it", "https://www.theguardian.com/us-news/2015/nov/24/donald-trump-on-waterboarding-even-if-it-doesnt-work-they-deserve-it"]
+    }
+  ],
+  crazy: [
+    {
+      title: "global warming is a hoax pepetrated by the Chinese",
       urls: [
         "https://twitter.com/realDonaldTrump/status/265895292191248385?ref_src=twsrc%5Etfw"
       ],
@@ -23,16 +45,23 @@ var data = {
       }
     },
     {
+      title: "temperatures are at record lows",
+      full_title: "Global Warming is \"bullshit\" and temperatures are at record lows",
+      urls: [
+        "https://twitter.com/realDonaldTrump/status/418542137899491328?ref_src=twsrc%5Etfw"
+      ]
+    },
+    {
       title: "environmentally friendly light bulbs cause cancer",
       urls: [ "https://twitter.com/realDonaldTrump/status/258593090107998208?ref_src=twsrc%5Etfw" ],
-      debunk: {         
+      debunk: {
         title: "They don't",
         urls: [ "http://www.ledlights.org/FAQ/Do-LED-Lights-Cause-Cancer.html", "http://energyblog.nationalgeographic.com/2014/01/08/separating-myth-from-fact-on-cfls-and-leds-five-concerns-addressed/comment-page-1/"]
       }
     },
     {
-      title: "wind turbines are awful because they kill eagles",
-      urls: ["https://twitter.com/realdonaldtrump/status/239088515122614272"]
+      title: "wind power kills \"all the birds\"",
+      urls: ["https://www.youtube.com/watch?v=2rSTwnKxjbY"]
     },
     {
       title: "asbestos is safe",
@@ -43,6 +72,7 @@ var data = {
     },
     {
       title: "asbestos would have prevented the World Trade Center from collapsing",
+      full_title: "asbestos would have prevented the World Trade Center from collapsing",
       urls: ["https://www.youtube.com/watch?v=sk3JUpDf5dk"]
     },
     {
@@ -50,14 +80,19 @@ var data = {
       urls: ["https://twitter.com/realDonaldTrump/status/449525268529815552?ref_src=twsrc%5Etfw"]
     },
     {
-      title: "doctors are lying about vaccines not causing autism",
+      title: "and doctors are lying about vaccines not causing autism",
+      full_title: "doctors are lying about vaccines not causing autism",
       urls: ["https://twitter.com/realDonaldTrump/status/507158574670573568?ref_src=twsrc%5Etfw"]
     },
     {
-      title: "Ted Cruz’s father was involved in JFK's assasination",
+      title: "Ted Cruz’s dad was involved in JFK's assasination",
       urls: [
         "http://www.politico.com/blogs/2016-gop-primary-live-updates-and-results/2016/05/trump-ted-cruz-father-222730"
       ]
+    },
+    {
+      title: "the unemployment rate might be 42%",
+      urls: ["http://www.politifact.com/truth-o-meter/statements/2015/sep/30/donald-trump/donald-trump-says-unemployment-rate-may-be-42-perc/"]
     },
     {
       title: "Obama forged his birth certificate",
@@ -68,7 +103,7 @@ var data = {
       ]
     },
     {
-      title: "Obama is secret Muslim",
+      title: "Obama is a Secret Muslim",
       urls: ["http://mediamatters.org/research/2015/09/18/from-fox-to-the-campaign-trail-trump-previously/205642"]
     },
     {
@@ -76,18 +111,19 @@ var data = {
       urls: ["http://mediamatters.org/blog/2015/06/16/the-worst-of-donald-trumps-media-commentary/204012"]
     },
     {
-      title: "Obama's birth name is ‘Barry Soetoro’",
+      title: "Obama was born ‘Barry Soetoro’",
+      full_title: "Obama's birth name is ‘Barry Soetoro’",
       urls: ["https://twitter.com/realDonaldTrump/status/525492766366007296", "http://www.foxnews.com/transcript/2011/04/15/exclusive-donald-trump-rips-into-president-obama-past/"]
     },
     {
-      title: "Obama murdered the Hawaiian State Health Director and staged it as a plane crash",
+      title: "Obama murdered the Hawaiian State Health Director and staged his death as a plane crash",
       urls:["https://twitter.com/realDonaldTrump/status/411247268763676673?ref_src=twsrc%5Etfw"]
     },
     {
-      title: "79 year old Scalia was murdered",
+      title: "Scalia was murdered",
+      full_title: "79 year old Scalia was murdered",
       urls: ["http://www.rightwingwatch.org/content/donald-trump-raises-suspicions-about-justice-scalias-death"]
     },
-    
   ]
 }
 
